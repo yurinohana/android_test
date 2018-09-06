8ed724726bc8a6a0308b258c847eceabb56ec429
yurinohana <yurinohanarshi@gmail.com>
2018-09-07 01:48:03 +0900

    first commit

---
+++ app/src/main/java/com/example/yuria/janken/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.yuria.janken
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        gu.setOnClickListener { onJankenButtonTapped(it) }
+        choki.setOnClickListener { onJankenButtonTapped(it) }
+        pa.setOnClickListener { onJankenButtonTapped(it) }
+    }
+
+    fun onJankenButtonTapped(view: View?) {
+        val intent = Intent(this, ResultActivity::class.java)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/example/yuria/janken/ResultActivity.kt
@@ -0,0 +1,12 @@
+package com.example.yuria.janken
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
